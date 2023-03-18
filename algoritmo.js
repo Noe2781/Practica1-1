8e80849a6d8e7146e0cca3af747c136f66412cd1
Noe2781 <108550273+Noe2781@users.noreply.github.com>
2023-03-18 02:17:54 -0300

    Add files via upload

---
+++ algoritmo.js
@@ -0,0 +1,7 @@
+document.querySelector("body").innerHTML = `
+<h1>Bienvenidos a mi pagina</h1>
+<h2>Donde vive el rock</h2>
+<h3>pronto mas novedades</h3>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdXT8EB-3acwr-O_vXqyXpozjG5aan9Ne6Kw&usqp=CAU" alt="La Renga">
+<img src="larenga.jpg" alt="chizo" srcset="">
+`;
